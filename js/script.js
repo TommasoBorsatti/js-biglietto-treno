8a2953f3b28dcec7084955c2afa61229710e21e9
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-02-26 18:07:25 +0100

    JS completato e raffinato
    Sistemato il problema di visualizzazione dovuto a un'istruzione scritta male (getElementByID); Aggiunto il controllo sui valori inseriti in modo che la pagina comunichi all'utente di non scrivere caratteri diversi da numeri

--- js/script.js
+++ js/script.js
@@ -3,31 +3,47 @@
 
 // 1. Quanti KM l'utente vuole percorrere
 
-var distanza = prompt("Salve. Grazie per aver scelto di viaggiare con noi. Dichiarare il numero dei KM che si intende percorrere.");
+var distanza = prompt("Salve e grazie per aver scelto di viaggiare con noi! Si prega di dichiarare il numero di KM relativi alla tratta che si intende percorrere.");
+
+var controllo = true;
+
+if (isNaN(distanza)) {
+  controllo = false;
+  document.getElementById("main-container").innerHTML = "<h1>ATTENZIONE!</h1><br><h2>Si prega di scrivere soltanto un valore numerico: ricaricare la pagina per calcolare il prezzo del Vostro biglietto.</h2>";
+}
 
 // 2. Quanti anni di età l'utente dichiara di avere
 
-var eta = prompt("Bene, adesso dichiarare il numero di anni compiuti.");
+if (controllo) {
+  var eta = prompt("Bene, adesso dichiarare il numero di anni compiuti.");
+
+  if (isNaN(eta)) {
+    controllo = false;
+      document.getElementById("main-container").innerHTML = "<h1>ATTENZIONE!</h1><br><h2>Si prega di scrivere soltanto un valore numerico: ricaricare la pagina per calcolare il prezzo del Vostro biglietto.</h2>";
+  }
+
+}
 
 // 3. Definizione del prezzo base del biglietto in base ai KM percorsi
 
-var prezzoLordo = 0.21 * distanza;
+if (controllo){
+  var prezzoLordo = 0.21 * distanza;
+  // 4. Applicazione dello sconto in base all'età dichiarata
 
-// 4. Applicazione dello sconto in base all'età dichiarata
+  if (eta < 18) {
+    prezzoNetto = prezzoLordo * 0.8;
+  }
 
-if (eta < 18) {
-  prezzoNetto = prezzoLordo * 0.8;
+  else if (eta >= 65) {
+    prezzoNetto = prezzoLordo * 0.6;
+  }
+
+  else {
+    prezzoNetto = prezzoLordo;
+  }
+
+  // 5. Stampa del risultato finale con il prezzo base moltiplicato per la giusta percentuale di sconto
+
+  document.getElementById("prezzo-netto").innerHTML = prezzoNetto + "€";
+
 }
-
-else if (eta >= 65) {
-  prezzoNetto = prezzoLordo * 0.6;
-}
-
-else {
-  prezzoNetto = prezzoLordo;
-}
-
-// 5. Stampa del risultato finale con il prezzo base moltiplicato per la giusta percentuale di sconto
-
-// console.log("Il tuo biglietto costa " + prezzoNetto + "€");
-document.getElementByID("prezzo-netto").innerHTML = "Il tuo biglietto costa " + prezzoNetto + "€";
