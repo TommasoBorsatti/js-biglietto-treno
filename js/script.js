d86b963b7ff6d2fa024d9f9690f9bce9931c993e
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-02-26 16:18:38 +0100

    Corretta la punteggiatura dello Script
    Mancavano tutti i ;

--- js/script.js
+++ js/script.js
@@ -3,30 +3,30 @@
 
 // 1. Quanti KM l'utente vuole percorrere
 
-var distanza = prompt("Salve. Grazie per aver scelto di viaggiare con noi. Dichiarare il numero dei KM che si intende percorrere");
+var distanza = prompt("Salve. Grazie per aver scelto di viaggiare con noi. Dichiarare il numero dei KM che si intende percorrere.");
 
 // 2. Quanti anni di età l'utente dichiara di avere
 
-var eta = prompt("Bene, adesso dichiarare il numero di anni compiuti.")
+var eta = prompt("Bene, adesso dichiarare il numero di anni compiuti.");
 
 // 3. Definizione del prezzo base del biglietto in base ai KM percorsi
 
-var prezzoLordo = 0.21 * distanza
+var prezzoLordo = 0.21 * distanza;
 
 // 4. Applicazione dello sconto in base all'età dichiarata
 
 if (eta < 18) {
-  prezzoNetto = prezzoLordo * 0.8
+  prezzoNetto = prezzoLordo * 0.8;
 }
 
 else if (eta >= 65) {
-  prezzoNetto = prezzoLordo * 0.6
+  prezzoNetto = prezzoLordo * 0.6;
 }
 
 else {
-  prezzoNetto = prezzoLordo
+  prezzoNetto = prezzoLordo;
 }
 
 // 5. Stampa del risultato finale con il prezzo base moltiplicato per la giusta percentuale di sconto
 
-console.log("Il tuo biglietto costa " + prezzoNetto + "€")
+console.log("Il tuo biglietto costa " + prezzoNetto + "€");
